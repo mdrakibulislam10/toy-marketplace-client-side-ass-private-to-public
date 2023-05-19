28d7700a6103a588e9e7068eedd1df0e09962ec7
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-05-20 00:48:44 +0600

    implement sign in with google

--- src/SocialLogin/SocialLogin.jsx
+++ src/SocialLogin/SocialLogin.jsx
@@ -1,11 +1,25 @@
+import { useContext } from "react";
 import { FaGoogle } from "react-icons/fa";
+import { AuthContext } from "../providers/AuthProviders/AuthProviders";
 
 const SocialLogin = () => {
+    const { signInWithGoogle } = useContext(AuthContext);
+
+    const handleGoogleSIgnIn = () => {
+        signInWithGoogle()
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    };
+
     return (
         <div className="mb-12">
             <div className="divider">OR</div>
             <div className="place-items-center">
-                <button className="flex flex-row flex-wrap justify-center gap-1 items-center card w-full border-2 border-sky-500 py-2 font-bold rounded-box">
+                <button onClick={handleGoogleSIgnIn} className="flex flex-row flex-wrap justify-center gap-1 items-center card w-full border-2 border-sky-500 py-2 font-bold rounded-box">
                     <FaGoogle /> Sign in with Google
                 </button>
             </div>
--- src/providers/AuthProviders/AuthProviders.jsx
+++ src/providers/AuthProviders/AuthProviders.jsx
@@ -1,9 +1,10 @@
 import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.config"
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProviders = ({ children }) => {
     const [user, setUser] = useState(null);
@@ -21,12 +22,25 @@ const AuthProviders = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password);
     };
 
+    // sign in with google
+    const signInWithGoogle = () => {
+        setSpinner(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+
     // logout
 
     // get user data
 
 
-    const authInfo = { user, spinner, signUp, login, };
+    const authInfo = {
+        user,
+        spinner,
+        signUp,
+        login,
+        signInWithGoogle,
+
+    };
 
     return (
         <AuthContext.Provider value={authInfo}>
